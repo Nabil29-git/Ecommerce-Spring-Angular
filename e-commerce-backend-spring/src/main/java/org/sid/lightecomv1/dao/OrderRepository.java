c65738383e57e58f16ae533e1f15f4c46cab9ff6
ELMAHDY <nabil.elmehdy@gmail.com>
2022-08-01 11:48:21 +0100

    initial commit

---
+++ e-commerce-backend-spring/src/main/java/org/sid/lightecomv1/dao/OrderRepository.java
@@ -0,0 +1,14 @@
+package org.sid.lightecomv1.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import javax.persistence.criteria.Order;
+
+@CrossOrigin("*")
+@RepositoryRestResource
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
